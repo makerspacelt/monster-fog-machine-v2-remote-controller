a7f2e66d423cc365d844c2f96318f6df0c478556
Kulverstukas <kulverstukas.k@gmail.com>
2023-07-20 20:57:54 +0300

    almost completely rewrote the code for better performance and more intuitive usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Ewma.h>
+#include <TinyWireM.h>
+#include <MagicPot.h>
 
 #define TINY4KOLED_QUICK_BEGIN
 #include <Tiny4kOLED.h>
@@ -11,74 +10,71 @@
 #define SWITCH_PIN PB1
 #define DURATION_POT A2
 #define INTERVAL_POT A3
-#define TIME_FACTOR 120 // increase this to add more time
+#define DURATION_MIN_READ 1
+#define DURATION_MAX_READ 60
+#define INTERVAL_MIN_READ 1
+#define INTERVAL_MAX_READ 30
+#define DURATION_STR "DURATION s:"
+#define INTERVAL_STR "INTERVAL s:"
 
-long duration;
-long interval;
+MagicPot durationPot(DURATION_POT, DURATION_MIN_READ, DURATION_MAX_READ);
+MagicPot intervalPot(INTERVAL_POT, INTERVAL_MIN_READ, INTERVAL_MAX_READ);
+
+int duration;
+int interval;
+int outputState;
 unsigned long durationMillis;
 unsigned long intervalMillis;
 unsigned long lastMillis;
-int outputState;
+unsigned long lastPollMillis;
 
-// needed for smoother ADC reading
-Ewma durationFilter(0.4);
-Ewma intervalFilter(0.4);
-
-void printDurationToScr(long secs) {
+void onDurationChange() {
+    duration = durationPot.getValue();
+    durationMillis = duration * 1000;
     oled.setCursor(96, 0);
-    oled.print(secs);
-    if (secs < 100) oled.print(" ");
+    oled.print(duration);
+    if (duration < 10) oled.print(" ");
 }
 
-void printIntervalToScr(int secs) {
+void onIntervalChange() {
+    interval = intervalPot.getValue();
+    intervalMillis = interval * 1000;
     oled.setCursor(96, 2);
-    oled.print(secs);
-    if (secs < 100) oled.print(" ");
-}
-
-void setupLcd() {
-    oled.begin();
-    oled.clear();
-    oled.setFont(FONT8X16);
-    oled.setCursor(0, 0);
-    oled.print("DURATION s:");
-    oled.setCursor(0, 2);
-    oled.print("INTERVAL s:");
-    // oled.print(oled.getCursorX());
-    oled.on();
-}
-
-long calcMillis(long val) {
-    long t = ceil((val * TIME_FACTOR) / 1000);
-    return ((t < 1) ? 1 : t) * 1000;
-}
-
-void processInput() {
-    duration = durationFilter.filter(analogRead(DURATION_POT)+1);
-    duration = (duration < 8) ? 8 : duration;
-    interval = intervalFilter.filter(analogRead(INTERVAL_POT)+1);
-    interval = (interval < 8) ? 8 : interval;
-
-    /*
-        we must divide the final millis by 2 because the internal clock somehow
-        reports twice as much millis and so timekeeping becomes very inaccurate
-    */
-    durationMillis = calcMillis(duration) / 2;
-    printDurationToScr((durationMillis * 2) / 1000);
-    intervalMillis = calcMillis(interval) / 2;
-    printIntervalToScr((intervalMillis * 2) / 1000);
+    oled.print(interval);
+    if (interval < 10) oled.print(" ");
 }
 
 void setup() {
-    setupLcd();
     pinMode(SWITCH_PIN, OUTPUT);
     pinMode(DURATION_POT, INPUT);
     pinMode(INTERVAL_POT, INPUT);
     digitalWrite(SWITCH_PIN, LOW);
+
+    oled.begin();
+    oled.clear();
+    oled.setFont(FONT8X16);
+    oled.setCursor(0, 0);
+    oled.invertOutput(true);
+    oled.print(DURATION_STR);
+    oled.setCursor(0, 2);
+    oled.invertOutput(false);
+    oled.print(INTERVAL_STR);
+    // oled.print(oled.getCursorX());
+    oled.on();
+
+    durationPot.begin();
+    durationPot.onChange(onDurationChange);
+    intervalPot.begin();
+    intervalPot.onChange(onIntervalChange);
 }
 
 void loop() {
-    processInput();
+    // prevent ultra fast polling for less noise
+    if ((unsigned long)millis() - lastPollMillis >= 50) {
+        durationPot.read();
+        intervalPot.read();
+        lastPollMillis = millis();
+    }
 
     bool changeState = false;
     if ((outputState == LOW) && (unsigned long)(millis() - lastMillis) >= intervalMillis) changeState = true;
@@ -88,7 +84,15 @@ void loop() {
         outputState = (outputState == LOW) ? HIGH : LOW;
         digitalWrite(SWITCH_PIN, outputState);
         lastMillis = millis();
-    }
 
-    
+        oled.setCursor(0, 0);
+        oled.invertOutput((outputState == HIGH));
+        oled.print(DURATION_STR);
+
+        oled.setCursor(0, 2);
+        oled.invertOutput((outputState == LOW));
+        oled.print(INTERVAL_STR);
+        
+        oled.invertOutput(false);
+    }
 }
